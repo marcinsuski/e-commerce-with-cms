d9605db6e175a70c7ad6bf525f78ace231e42eeb
Marcin Suski <114236312+marcinsuski@users.noreply.github.com>
2023-04-21 15:30:16 +0200

    login with google

--- admin/pages/index.tsx
+++ admin/pages/index.tsx
@@ -1,20 +1,46 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 export default function Home() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>ecommerce Admin panel</title>
-                <meta name="description" content="Admin panel" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+    const { data: session } = useSession();
+    if (!session) {
+        return (
+            <div className={styles.container}>
+                <Head>
+                    <title>ecommerce Admin panel</title>
+                    <meta name="description" content="Admin panel" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
 
-            <main className={styles.main}>
-                <div className={styles.btn_wrapper}>
-                    <button data-type="login-btn">Login with Google</button>
-                </div>
-            </main>
-        </div>
-    );
+                <main className={styles.main}>
+                    <div className={styles.btn_wrapper}>
+                        <button
+                            data-type="login-btn"
+                            onClick={() => signIn("google")}
+                        >
+                            Login with Google
+                        </button>
+                    </div>
+                </main>
+            </div>
+        );
+    }
+    if (session) {
+        return (
+            <div className={styles.container}>
+                <Head>
+                    <title>ecommerce Admin panel</title>
+                    <meta name="description" content="Admin panel" />
+                    <link rel="icon" href="/favicon.ico" />
+                </Head>
+
+                <main className={styles.main}>
+                    <div className={styles.btn_wrapper}>
+                        <div>Logged in {session.user?.email}</div>
+                    </div>
+                </main>
+            </div>
+        );
+    }
 }
